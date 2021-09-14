b0cc8b424fb671099707dd6dcdb6519f91e93e42
gabg4224 <gabg4224@gmail.com>
2021-09-14 00:32:41 -0300

    fase casi definitiva

---
+++ dom.js
@@ -0,0 +1,176 @@
+let container = document.querySelector("#container"),
+  header = document.querySelector("#header"),
+  main = document.querySelector("#main"),
+  footer = document.querySelector("#footer");
+
+//---------------------Header-------------------------------
+let titleContainer = document.createElement("DIV"),
+titulo = document.createElement("H1");
+
+titleContainer.classList.add("titleContainer");
+titulo.classList.add("title");
+
+titulo.innerHTML = "Let's play SUCKERS!!";
+header.appendChild(titleContainer);
+titleContainer.appendChild(titulo);
+
+//------------------------Main-------------------------------
+let pointsContainer = document.createElement("DIV"),
+  pointsPlayerContainer = document.createElement("DIV"),
+  playerName = document.createElement("H3"),
+  playerContainer = document.createElement("DIV"),
+  playerBoxPoints = document.createElement("DIV"),
+  playerElection = document.createElement("SPAN"),
+  pointsEnemyContainer = document.createElement("DIV"),
+  enemyName = document.createElement("H3"),
+  enemyContainer = document.createElement("DIV"),
+  enemyBoxPoints = document.createElement("DIV"),
+  enemyElection = document.createElement("SPAN");
+
+//---------------------Main.first-------------------------------
+
+pointsContainer.classList.add("pointsContainer");
+pointsPlayerContainer.classList.add("pointsPlayerContainer");
+playerContainer.classList.add("playerContainer");
+playerName.classList.add("playerName");
+playerBoxPoints.classList.add("playerBoxPoints");
+playerElection.classList.add("playerElection");
+pointsEnemyContainer.classList.add("pointsEnemyContainer");
+enemyContainer.classList.add("enemyContainer");
+enemyName.classList.add("enemyName");
+enemyBoxPoints.classList.add("enemyBoxPoints");
+enemyElection.classList.add("enemyElection");
+
+
+pointsPlayerContainer.appendChild(playerContainer);
+playerContainer.appendChild(playerName);
+playerContainer.appendChild(playerBoxPoints);
+playerContainer.appendChild(playerElection);
+
+pointsEnemyContainer.appendChild(enemyContainer);
+enemyContainer.appendChild(enemyName);
+enemyContainer.appendChild(enemyBoxPoints);
+enemyContainer.appendChild(enemyElection);
+
+pointsContainer.appendChild(pointsPlayerContainer);
+pointsContainer.appendChild(pointsEnemyContainer);
+
+main.appendChild(pointsContainer);
+
+//---------------------------Main.First.Player---------------------------------------
+
+playerName.textContent = `player:`;
+playerBoxPoints.textContent = playerPoint;
+playerElection.innerHTML = ``;
+
+//---------------------------Main.First.Enemy---------------------------------------
+
+enemyName.textContent = `enemy:`;
+enemyBoxPoints.textContent = playerPoint;
+enemyElection.innerHTML = ``;
+
+//---------------------Main.second-------------------------------
+let boxContainer = document.createElement("DIV"),
+  rockBoxContainer = document.createElement("DIV"),
+  rockBox = document.createElement("SPAN"),
+  paperBoxContainer = document.createElement("DIV"),
+  paperBox = document.createElement("SPAN"),
+  scissorBoxContainer = document.createElement("DIV"),
+  scissorBox = document.createElement("SPAN");
+
+boxContainer.classList.add("boxContainer");
+rockBoxContainer.classList.add("btnBox");
+rockBox.classList.add("figBox");
+paperBoxContainer.classList.add("btnBox");
+paperBox.classList.add("figBox");
+scissorBoxContainer.classList.add("btnBox");
+scissorBox.classList.add("figBox");
+
+rockBoxContainer.appendChild(rockBox);
+paperBoxContainer.appendChild(paperBox);
+scissorBoxContainer.appendChild(scissorBox);
+
+boxContainer.appendChild(rockBoxContainer);
+boxContainer.appendChild(paperBoxContainer);
+boxContainer.appendChild(scissorBoxContainer);
+
+main.appendChild(boxContainer);
+
+rockBox.innerHTML = `<img src="..//rock_paper-scissors/rock.svg" alt="img de tijera" width = 60px>`;
+paperBox.innerHTML = `<img src="..//rock_paper-scissors/paper.svg" alt="img de tijera" width = 60px>`;
+scissorBox.innerHTML = `<img src="..//rock_paper-scissors/scissors.svg" alt="img de tijera" width = 60px>`;
+
+//---------------------Footer--------------------------------
+
+let messageContainer = document.createElement("div"),
+  message = document.createElement("SPAN");
+
+messageContainer.classList.add("messageContainer");
+message.classList.add("message");
+
+messageContainer.appendChild(message);
+footer.appendChild(messageContainer);
+
+message.innerHTML = `<h2>welcome to the arena!</h2>`;
+
+//---------------------Reset--------------------------------
+
+let resetContainer = document.querySelector("#resetContainer"),
+  resetModal = document.querySelector("#resetModal"),
+  resetTitle = document.querySelector("#resetTitle"),
+  resetContent = document.querySelector("#resetContent"),
+  resetBtn = document.querySelector("#resetBtn");
+
+/*----------------------------------------------------------- */
+
+rockBox.addEventListener("click", () => {
+  let pase = game("rock");
+  playerElection.innerHTML = `<img src="..//rock_paper-scissors/rock.svg" alt="roca">`;
+  enemyElection.innerHTML = `<img src="..//rock_paper-scissors/${compuSelect}.svg" alt="${compuSelect}">`;
+  pase == "tie"
+    ? (message.innerHTML = `<h2>it's a TIE!!</h2>`)
+    : (message.innerHTML = `<h2>${pase}</h2>`);
+
+  playerBoxPoints.innerHTML = playerPoint;
+  enemyBoxPoints.innerHTML = enemyPoint;
+  resetGame();
+});
+
+paperBox.addEventListener("click", () => {
+  let pase = game("paper");
+  playerElection.innerHTML = `<img src="..//rock_paper-scissors/paper.svg" alt="roca">`;
+  enemyElection.innerHTML = `<img src="..//rock_paper-scissors/${compuSelect}.svg" alt="${compuSelect}">`;
+
+  pase == "tie"
+    ? (message.innerHTML = `<h2>it's a TIE!!</h2>`)
+    : (message.innerHTML = `<h2>${pase}</h2>`);
+  playerBoxPoints.innerHTML = playerPoint;
+  enemyBoxPoints.innerHTML = enemyPoint;
+  resetGame();
+});
+
+scissorBox.addEventListener("click", () => {
+  let pase = game("scissors");
+  playerElection.innerHTML = `<img src="..//rock_paper-scissors/scissors.svg" alt="roca">`;
+  enemyElection.innerHTML = `<img src="..//rock_paper-scissors/${compuSelect}.svg" alt="${compuSelect}">`;
+
+  pase == "tie"
+    ? (message.innerHTML = `<h2>it's a TIE!!</h2>`)
+    : (message.innerHTML = `<h2>${pase}</h2>`);
+
+  playerBoxPoints.innerHTML = playerPoint;
+  enemyBoxPoints.innerHTML = enemyPoint;
+
+  resetGame();
+});
+
+resetBtn.addEventListener("click", () => {
+  playerPoint = 0;
+  enemyPoint = 0;
+  playerBoxPoints.innerHTML = playerPoint;
+  enemyBoxPoints.innerHTML = enemyPoint;
+  playerElection.innerHTML = ''
+  enemyElection.innerHTML = ''
+  message.innerHTML = `<h2>welcome to the arena!</h2>`;
+  resetContainer.classList.toggle("show");
+});
--- script.js
+++ script.js
@@ -1,43 +1,103 @@
 let computerPlay = () => {
-    let numero = Math.floor(Math.random() * 100)
+  let numero = Math.floor(Math.random() * 100);
 
-    if (numero >= 1 && numero < 33) {
-        return 'rock'
-    } else if (numero >= 33 && numero < 66) {
-        return 'paper'
-    } else if (numero >= 66 && numero <= 99) {
-        return 'scissors'
-    }
+  if (numero >= 1 && numero < 33) {
+    return "rock";
+  } else if (numero >= 33 && numero < 66) {
+    return "paper";
+  } else if (numero >= 66 && numero <= 99) {
+    return "scissors";
+  }
+};
+
+let playerPoint = 0;
+let enemyPoint = 0;
+
+let compuSelect = ''
 
 
-}
+let game = (playerElection) => {
+  let player = playerElection;
+  //let player = playerSelect.toLowerCase();
+  let computadora = computerPlay()
+  compuSelect = computadora
+  console.log(`you choose ${player}, and computer, ${computadora}`);
+  
+  if (player == "rock" && computadora == "scissors") {
+    playerPoint++;
+    return noticePlayer();
+  } else if (player == "rock" && computadora == "paper") {
+    enemyPoint++;
+    return noticeEnemy();
+  }
 
-let game = (playerSelect) => {
+  if (player == "paper" && computadora == "rock") {
+    playerPoint++;
+    return noticePlayer();
+  } else if (player == "paper" && computadora == "scissors") {
+    enemyPoint++;
+    return noticeEnemy();
+  }
 
-    let player = playerSelect.toLowerCase()
-    let computadora = computerPlay()
+  if (player == "scissors" && computadora == "paper") {
+    playerPoint++;
+    return noticePlayer();
+  } else if (player == "scissors" && computadora == "rock") {
+    enemyPoint++;
+    return noticeEnemy();
+  }
 
-    console.log( `you choose ${player}, and computer, ${computadora}`)
+  if (player == computadora) {
+    return `tie`;
+  }
+};
 
-    if(player == computadora){
-        return 'draw! play again'
-    }
+let noticePlayer = () => {
+  if (playerPoint === 1) {
+    return "jugador ha anotado";
+  } else if (playerPoint === 2) {
+    return "jugador ha anotado otro punto";
+  } else if (playerPoint === 3) {
+    return "ya estamos cerca de conseguirlo";
+  } else if (playerPoint === 4) {
+    return "punto gana!";
+  } else if (playerPoint === 5) {
+   
+    return  'haz ganado la contienda jugador'
+  }
+};
 
-    if (player == 'rock' && computadora == 'scissors') {
-        return 'you won'
-    } else if (player == 'rock' && computadora == 'paper') {
-        return `you lose! ${computadora} beat ${player}`
-    }
+let noticeEnemy = () => {
+  if (enemyPoint === 1) {
+    return "Punto para el bato";
+  } else if (enemyPoint === 2) {
+    return "el bato lo hace de nuevo y el publico enloquece!!";
+  } else if (enemyPoint === 3) {
+    return "anotacioooon del bato!!";
+  } else if (enemyPoint === 4) {
+    return "punto gana para el bato";
+  } else if (enemyPoint === 5) {
+    
+    return 'a chingar a su madre el bato mandaaa'
+   ;
+  }
+};
 
-    if (player == 'paper' && computadora == 'rock') {
-        return 'you won'
-    } else if (player == 'paper' && computadora == 'scissors') {
-        return `you lose! ${computadora} beat ${player}`
-    }
 
-    if (player == 'scissors' && computadora == 'paper') {
-        return 'you won'
-    } else if (player == 'scissors' && computadora == 'rock') {
-        return `you lose! ${computadora} beat ${player}`
-    }
-}
+
+let resetGame = () => {
+
+
+  if (playerPoint == 5) {
+    resetContainer.classList.toggle("show");
+    resetTitle.textContent = "haz ganado la contienda jugador";
+    resetContent.textContent ="demostraste tu valia, pero crees poder hacerlo de nuevo? prueba a intentarlo nuevamente"
+    resetBtn.textContent = 'Volver a la arena'
+
+  }else if(enemyPoint == 5){
+    resetContainer.classList.toggle("show");
+    resetTitle.textContent = "Mejor suerte para la proxima";
+    resetContent.innerHTML ="Parece que esta vez no ha habido suerte jugador, continua intentandolo, seguro que si te esfuerzas... la proxima vez no perderas tan misearablemente :3 <br><b>ATT. El desarrollador<b/>"
+    resetBtn.textContent = 'Revancha'
+  }
+};
